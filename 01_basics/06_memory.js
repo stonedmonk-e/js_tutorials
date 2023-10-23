83633920cafdccd7c0a457dccad6e5db3615bc2e
Shashank S <111871083+stonedmonk-e@users.noreply.github.com>
2023-10-23 04:16:44 +0000

    stack & heap

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,35 @@
+// Stack(Primitive), Heap (Non-primitve) memory
+
+let myName = "shashank";
+
+let anotherName = myName; // copied to anotherName
+anotherName = "bangaru"; // so changing this won't make any difference to myName
+
+console.log(myName); 
+console.log(anotherName);
+
+/* output
+shashank
+bangaru
+*/
+
+// ----------------------------------------------------------- //
+
+// Objects - stored in heap
+let user1 = {
+    email : "user@goog.com",
+    upi : "user@sbl"
+}
+
+//when changed, only reference is passed 
+let user2 = user1
+user2.email = "bangaru@goog.com" // change user2 value
+
+// reference is changed, so both values will be changed
+console.log(user1.email);
+console.log(user2.email);
+
+/* output
+bangaru@goog.com
+bangaru@goog.com
+*/
