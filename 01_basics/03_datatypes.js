45f6fed0fae69a136b1a35a572edc4fce12bc38d
Shashank S <111871083+stonedmonk-e@users.noreply.github.com>
2023-10-23 04:16:30 +0000

    primitive & reference

--- 01_basics/03_datatypes.js
+++ 01_basics/03_datatypes.js
@@ -19,8 +19,57 @@ string => ""
 boolean => true/false
 null => standalone value
 undefined => 
-symbol => used mostly in react to fin uniqueness of components
+symbol => used mostly in react to find uniqueness of components
 */
 
 console.log(typeof null); // null is an object
 console.log(typeof undefined); // this is just undefined
+
+// -------------------------------------------------------
+
+// Primitive Datatypes
+
+// 7 categories : String, Number, null, Boolean, undefined, Symbol, BigInt
+
+// Reference / Non-Primtive Datatypes : Array, Objects, Functions
+
+// uniquely defined
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // prints false
+
+// BigInt, we use 'n' at end
+const bigNumber = 257800005278418451156984n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "Nagraj", "Doga"];
+
+let myObj = {
+    name : "snoopy",
+    age: 22,
+}
+
+const myFunc = function(){
+    console.log("Hellooo!!!");
+}
+
+// --------------------------------------------------- //
+
+// typeof => to check data type
+console.log( typeof(bigNumber) )
+
+console.log(typeof 42);
+// Expected output: "number"
+
+console.log(typeof 'blubber');
+// Expected output: "string"
+
+console.log(typeof true);
+// Expected output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// Expected output: "undefined"
