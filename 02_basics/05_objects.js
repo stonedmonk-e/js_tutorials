7eedf256ab01085898a84fb9fb41f23837fe5577
Shashank S <111871083+stonedmonk-e@users.noreply.github.com>
2023-11-08 05:42:43 +0000

    objects destructuring and JSON APIs

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,110 @@
+//  objects destructuring and JSON APIs
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor => this is how we usaully access the data
+// but writing this again and again is not efficient, so
+
+const {courseInstructor} = course
+console.log(courseInstructor); // we can directly access like this
+
+const {courseInstructor : instructor} = course
+console.log(instructor); // we can directly access like this in short form too
+
+/* React example - 
+const navbar = ({company}) => {
+
+}
+// here object destructuring using braces
+navbar(company = "hitesh")
+*/
+
+/* 
+// APIs
+{
+    "name" : "shashank",
+    "coursename" : "js in hindi",
+    "price" : "free"
+}
+
+[
+    {},
+    {},
+    {}
+]
+
+// randomuserme api
+
+{
+    "results": [
+      {
+        "gender": "female",
+        "name": {
+          "title": "Miss",
+          "first": "Jennie",
+          "last": "Nichols"
+        },
+        "location": {
+          "street": {
+            "number": 8929,
+            "name": "Valwood Pkwy",
+          },
+          "city": "Billings",
+          "state": "Michigan",
+          "country": "United States",
+          "postcode": "63104",
+          "coordinates": {
+            "latitude": "-69.8246",
+            "longitude": "134.8719"
+          },
+          "timezone": {
+            "offset": "+9:30",
+            "description": "Adelaide, Darwin"
+          }
+        },
+        "email": "jennie.nichols@example.com",
+        "login": {
+          "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+          "username": "yellowpeacock117",
+          "password": "addison",
+          "salt": "sld1yGtd",
+          "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+          "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+          "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+        },
+        "dob": {
+          "date": "1992-03-08T15:13:16.688Z",
+          "age": 30
+        },
+        "registered": {
+          "date": "2007-07-09T05:51:59.390Z",
+          "age": 14
+        },
+        "phone": "(272) 790-0888",
+        "cell": "(489) 330-2385",
+        "id": {
+          "name": "SSN",
+          "value": "405-88-3636"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/75.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+        },
+        "nat": "US"
+      }
+    ],
+    "info": {
+      "seed": "56d27f4a53bd5441",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }
+
+  // now use a json formatter to make it look better
+  */
