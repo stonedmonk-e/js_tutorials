25c469dfbd0731b107af7d9b86158faebe03d2ef
Shashank S <111871083+stonedmonk-e@users.noreply.github.com>
2023-11-12 02:55:17 +0000

    switch-case

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,21 @@
+
+month = 7
+
+switch (month) {
+    case 1: // we can use string like [ case "jan": ]
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("NOTA");
+        break;
+}
