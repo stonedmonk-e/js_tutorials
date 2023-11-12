6d53cf064daaf0b2df2411cb4024568a335e10c4
Shashank S <111871083+stonedmonk-e@users.noreply.github.com>
2023-11-12 03:38:18 +0000

    while, do while

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,24 @@
+/*
+let index = 0;
+while(index  <= 10){
+    console.log(`Value of index is ${index}`);
+    index += 2;
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let i = 0;
+
+while( i < myArray.length){
+    console.log(`Value is ${myArray[i]}`);
+    i++;
+}
+*/
+
+//let score = 1
+let score = 11 // even then it runs once
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10);
